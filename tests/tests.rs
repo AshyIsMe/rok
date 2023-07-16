6c7911ed6536e81691909c357bf3e42b566cdc8e
Aaron Ash <aaron.ash@gmail.com>
2023-07-17 00:22:31 +1000

    feat: parens

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,20 +3,20 @@ use rok::*;
 
 use rok::KW::*;
 
+macro_rules! arr {
+    ($v:expr) => {
+        Series::new("", $v)
+    };
+}
+
 #[test]
 fn test_1() {
     assert_eq!(scan("1").unwrap(), vec![Noun(K::Bool(1u8))]);
     assert_eq!(scan("2").unwrap(), vec![Noun(K::Int(Some(2)))]);
     assert_eq!(scan("3.14").unwrap(), vec![Noun(K::Float(3.14))]);
-    assert_eq!(
-        scan("1 0 1 0 1").unwrap(),
-        vec![Noun(K::BoolArray(Series::new("", [1, 0, 1, 0, 1u8])))]
-    );
-    assert_eq!(scan("1 2 3").unwrap(), vec![Noun(K::IntArray(Series::new("", [1, 2, 3i64])))]);
-    assert_eq!(
-        scan("1 2 3.14").unwrap(),
-        vec![Noun(K::FloatArray(Series::new("", [1., 2., 3.14])))]
-    );
+    assert_eq!(scan("1 0 1 0 1").unwrap(), vec![Noun(K::BoolArray(arr!([1, 0, 1, 0, 1u8])))]);
+    assert_eq!(scan("1 2 3").unwrap(), vec![Noun(K::IntArray(arr!([1, 2, 3i64])))]);
+    assert_eq!(scan("1 2 3.14").unwrap(), vec![Noun(K::FloatArray(arr!([1., 2., 3.14])))]);
 }
 
 #[test]
@@ -39,8 +39,18 @@ fn test_2() {
 
     assert_eq!(
         eval(scan("1 0 1 0 1 + 2").unwrap()).unwrap(),
-        Noun(K::IntArray(Series::new("", [3, 2, 3, 2, 3])))
+        Noun(K::IntArray(arr!([3, 2, 3, 2, 3])))
     );
 
-    // assert_eq!(eval(scan("1 2 3 + 4 5 6").unwrap()).unwrap(), Noun(K::IntArray(Series::new("", [5, 7, 9]))));
+    // assert_eq!(eval(scan("1 2 3 + 4 5 6").unwrap()).unwrap(), Noun(K::IntArray(arr!([5, 7, 9]))));
+}
+
+#[test]
+fn test_3() {
+    assert_eq!(scan("(1)").unwrap(), vec![KW::LP, Noun(K::Bool(1)), KW::RP]);
+    assert_eq!(eval(scan("(1)").unwrap()).unwrap(), Noun(K::Bool(1)));
+    assert_eq!(eval(scan("(1+1)").unwrap()).unwrap(), Noun(K::Int(Some(2))));
+    assert_eq!(eval(scan("(1+1)+3").unwrap()).unwrap(), Noun(K::Int(Some(5))));
+    assert_eq!(eval(scan("(1*1)+(3*3)").unwrap()).unwrap(), Noun(K::Int(Some(10))));
+    assert_eq!(eval(scan("((2 + 0 + 0 + 1 - 1) * (3*1)) + 1").unwrap()).unwrap(), Noun(K::Int(Some(7))));
 }
