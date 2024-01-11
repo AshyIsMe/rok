362618f0bfaed9c598a2d625918ef50cb2963f03
Aaron Ash <aaron.ash@gmail.com>
2024-01-11 23:25:30 +1000

    test: dict equals test

--- tests/tests.rs
+++ tests/tests.rs
@@ -631,4 +631,12 @@ fn test_equal() {
   let res = eval(&mut env, scan("1 2 3 = 1 2 3").unwrap()).unwrap();
   println!("res: {:?}", res);
   assert_eq!(res, Noun(K::BoolArray(arr!([1, 1, 1u8]))));
+
+  let res = eval(&mut env, scan("1 2 3 = `a`b`c!(1;2;3)").unwrap()).unwrap();
+  println!("res: {:?}", res);
+  let k =
+    K::SymbolArray(Series::new("a", ["a", "b", "c"]).cast(&DataType::Categorical(None)).unwrap());
+  let v = K::List(vec![K::Bool(1u8), K::Int(Some(2)), K::Int(Some(3))]);
+  let d1 = v_makedict(k, v).unwrap();
+  assert_eq!(res, Noun(d1));
 }
