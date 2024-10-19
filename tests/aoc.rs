ef76e32c1ec297fb9a58ecbf75a6263d85b83fb1
Aaron Ash <aaron.ash@gmail.com>
2024-10-19 10:39:28 +1000

    test: aoc2015_12_01

---
+++ tests/aoc.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+use polars::prelude::*;
+use roklang::*;
+
+use roklang::KW::*;
+
+#[test]
+fn test_aoc2015_12_01() {
+  let mut env = Env { names: HashMap::new(), parent: None };
+
+  assert_eq!(
+    eval(&mut env, scan(r#"(+/-1*")"=s)+(+/"("=s:"()")"#).unwrap()).unwrap(),
+    Noun(K::Int(Some(0)))
+  );
+
+  // TODO
+  // assert_eq!(
+  //   eval(&mut env, scan(r#"+//1 -1*"()"=\:s:"((()))""#).unwrap()).unwrap(),
+  //   Noun(K::Int(Some(0)))
+  // );
+}
